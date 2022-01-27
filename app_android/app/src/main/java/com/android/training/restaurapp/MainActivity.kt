390c53aff5fc4fdbcbe008273e3e3e8f5177da80
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2022-01-26 21:44:43 -0600

    -
    Proyecto de prueba offline.

--- app_android/app/src/main/java/com/android/training/restaurapp/MainActivity.kt
+++ app_android/app/src/main/java/com/android/training/restaurapp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.android.training.restaurapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textViewHelloWorld)
+        textView.setText("RestaurAPP")
     }
 }
